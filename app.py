c383661143695fe94c17a7b1b3e1628e392130ae
aimanalishezan <aimanalishezan22@gmail.com>
2025-04-02 00:13:18 +0600

    try

--- app.py
+++ app.py
@@ -2,3 +2,21 @@ import ollama
 import requests
 
 MODEL=""
+
+system_prompt="you are a assistant who ans all kind of questions"
+question=input("Ask your qestion")
+
+user_prompt="answer the questions in short and best summary possible"+question
+
+def message():
+    return[
+        {"role":"system","content":system_prompt},
+        {"role":"user","content":user_prompt}
+    ]
+
+def llama():
+    response=ollama.chat(
+        model=MODEL,
+        messages=message()
+
+    )
